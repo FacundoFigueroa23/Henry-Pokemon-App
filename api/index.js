880e306b9cd0685c5c46b6b0e4feeb13b77ad1a7
facu <facu-figueroa-98@hotmail.com>
2021-10-22 17:26:05 -0300

    modify index.js file from /api

--- api/index.js
+++ api/index.js
@@ -20,9 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+server.set('port', (process.env.PORT || 3001))
+
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
+  server.listen(server.get('port'), () => {
+    console.log(`%s listening at ${server.get('port')}`); // eslint-disable-line no-console
   });
 });
