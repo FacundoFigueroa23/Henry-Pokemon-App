1ca27bca357a77119990a8c2d184dc2ecb179f31
FacundoFigueroa23 <facundoleonelfigueroa@hotmail.com>
2023-05-15 23:45:42 -0300

    Change constants

--- api/src/utils/constants.js
+++ api/src/utils/constants.js
@@ -1,11 +1,9 @@
 const POKEMONS_URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
-const POKEMON_ID = "https://pokeapi.co/api/v2/pokemon/";
-const POKEMON_NAME = "https://pokeapi.co/api/v2/pokemon/";
+const POKEMON = "https://pokeapi.co/api/v2/pokemon/";
 const POKEMONS_TYPES = "https://pokeapi.co/api/v2/type";
 
 module.exports = {
     POKEMONS_URL,
-    POKEMON_ID,
-    POKEMON_NAME,
+    POKEMON,
     POKEMONS_TYPES
 }
